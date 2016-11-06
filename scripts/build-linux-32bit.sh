faf29408601292f5607fe28cb0d6aa7d500da9e2
Artur Michałowski <amichalowski92@gmail.com>
2016-11-06 20:13:31 +0100

    create pack directory if not exist

--- scripts/build-linux-32bit.sh
+++ scripts/build-linux-32bit.sh
@@ -11,5 +11,6 @@ if [ $? -eq 0 ]; then
 	mkdir release
 	cp nsbuilder nsbuilder_pl.qm release/
 	cp release nsbuilder1-`cat SVN_VERSION_FILE`-linux86-bin
+	if [ ! -e pack ]; then mkdir pack; fi
 	zip pack/nsbuilder1-`cat SVN_VERSION_FILE`-linux86-bin.zip release/*
 fi
